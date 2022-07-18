8057a9346b1e827b28ee599c3b90d1148157946c
Maple8192 <57043077+maple8192@users.noreply.github.com>
2022-07-19 01:00:57 +0900

    :sparkles: 読み取ったコードからプログラム部分だけを抽出する処理の記述

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,28 @@ fn main() {
     let code_path = &args[1];
     let content = fs::read_to_string(code_path).unwrap();
 
-    println!("{}", content);
+    println!("{}", extract_code(content));
+}
+
+fn extract_code(raw_code: String) -> String {
+    let mut code: Vec<char> = Vec::new();
+
+    let mut in_comment = false;
+
+    for c in raw_code.chars() {
+        if c == '\n' {
+            in_comment = false;
+            continue;
+        }
+        if in_comment {
+            continue;
+        }
+        match c {
+            '>' | '<' | '+' | '-' | '[' | ']' | '.' | ',' => code.push(c),
+            '#' => in_comment = true,
+            _ => (),
+        }
+    }
+
+    code.iter().collect()
 }
