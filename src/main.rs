cae4d835e386a9f1606ada6917830950708dcf91
Maple8192 <57043077+maple8192@users.noreply.github.com>
2022-07-19 22:04:27 +0900

    :sparkles: プログラムの処理をenumで表すように変更

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,22 @@ fn main() {
     let code_path = &args[1];
     let content = fs::read_to_string(code_path).unwrap();
 
-    println!("{}", extract_code(content));
+    let code = extract_code(content);
 }
 
-fn extract_code(raw_code: String) -> String {
-    let mut code: Vec<char> = Vec::new();
+enum Token {
+    Inc,
+    Dec,
+    IncPtr,
+    DecPtr,
+    LoopIn,
+    LoopOut,
+    Print,
+    Read,
+}
+
+fn extract_code(raw_code: String) -> Vec<Token> {
+    let mut code: Vec<Token> = Vec::new();
 
     let mut in_comment = false;
 
@@ -20,15 +31,20 @@ fn extract_code(raw_code: String) -> String {
             in_comment = false;
             continue;
         }
-        if in_comment {
-            continue;
-        }
+        if in_comment { continue; }
         match c {
-            '>' | '<' | '+' | '-' | '[' | ']' | '.' | ',' => code.push(c),
+            '+' => code.push(Token::Inc),
+            '-' => code.push(Token::Dec),
+            '>' => code.push(Token::IncPtr),
+            '<' => code.push(Token::DecPtr),
+            '[' => code.push(Token::LoopIn),
+            ']' => code.push(Token::LoopOut),
+            '.' => code.push(Token::Print),
+            ',' => code.push(Token::Read),
             '#' => in_comment = true,
             _ => (),
         }
     }
 
-    code.iter().collect()
+    code
 }
