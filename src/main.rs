aecaaf08b8d14762e1ff16e51fd600dac3351b28
Maple8192 <57043077+maple8192@users.noreply.github.com>
2022-07-24 03:33:52 +0900

    :art: ソースファイルの中身からソースコードだけを抽出する処理をcode_processor.rsに分離

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod file_reader;
+mod code_processor;
 
 use std::env;
 
@@ -13,12 +14,12 @@ fn main() {
     let code_content = code_content.unwrap();
     let input_content = input_content.unwrap().unwrap();
 
-    let code = extract_code(code_content);
+    let code = code_processor::extract_code(code_content);
 
     run(code, input_content);
 }
 
-enum Token {
+pub enum Token {
     Inc,
     Dec,
     IncPtr,
@@ -27,34 +28,6 @@ enum Token {
     LoopOut,
     Print,
     Read,
-}
-
-fn extract_code(raw_code: String) -> Vec<Token> {
-    let mut code: Vec<Token> = Vec::new();
-
-    let mut in_comment = false;
-
-    for c in raw_code.chars() {
-        if c == '\n' {
-            in_comment = false;
-            continue;
-        }
-        if in_comment { continue; }
-        match c {
-            '+' => code.push(Token::Inc),
-            '-' => code.push(Token::Dec),
-            '>' => code.push(Token::IncPtr),
-            '<' => code.push(Token::DecPtr),
-            '[' => code.push(Token::LoopIn),
-            ']' => code.push(Token::LoopOut),
-            '.' => code.push(Token::Print),
-            ',' => code.push(Token::Read),
-            '#' => in_comment = true,
-            _ => (),
-        }
-    }
-
-    code
 }
 
 fn run(code: Vec<Token>, input: String) {
