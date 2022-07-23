9d81e43bfc66e00b8aed355b6c5bc1602cf337e9
Maple8192 <57043077+maple8192@users.noreply.github.com>
2022-07-24 04:57:49 +0900

    :construction: プログラム命令実行系をstructとしてまとめた(エラー処理未実装)

---
+++ src/interpreter.rs
@@ -0,0 +1,104 @@
+use std::error::Error;
+use crate::Token;
+
+pub struct Machine {
+    code: Vec<Token>,
+    input: Vec<char>,
+    program_pointer: usize,
+    pointer: isize,
+    memory: Vec<u8>,
+    input_pointer: usize,
+}
+
+impl Machine {
+    pub fn new(code: Vec<Token>, input: String) -> Self {
+        Machine {
+            code,
+            input: input.chars().collect(),
+            program_pointer: 0,
+            pointer: 0,
+            memory: vec![0],
+            input_pointer: 0,
+        }
+    }
+
+    fn increment(&mut self) {
+        self.memory[self.pointer as usize] = self.memory[self.pointer as usize].wrapping_add(1);
+    }
+
+    fn decrement(&mut self) {
+        self.memory[self.pointer as usize] = self.memory[self.pointer as usize].wrapping_sub(1);
+    }
+
+    fn increment_pointer(&mut self) {
+        self.pointer += 1;
+        if self.pointer >= self.memory.len() as isize {
+            self.memory.push(0);
+        }
+    }
+
+    fn decrement_pointer<E: Error>(&mut self) -> Result<(), E> {
+        self.pointer -= 1;
+        if self.pointer < 0 {
+            todo!()
+        }
+
+        Ok(())
+    }
+
+    fn is_zero<E: Error>(&self) -> bool {
+        self.memory[self.pointer as usize] == 0
+    }
+
+    fn jump_to_close_bracket<E: Error>(&mut self) -> Result<(), E> {
+        let mut count = 0;
+        for i in (self.program_pointer + 1)..=self.code.len() {
+            if i == self.code.len() {
+                todo!()
+            }
+
+            match self.code[i] {
+                Token::LoopIn => count += 1,
+                Token::LoopOut => if count == 0 {
+                    self.program_pointer = i;
+                    break;
+                } else {
+                    count -= 1;
+                }
+                _ => (),
+            }
+        }
+
+        Ok(())
+    }
+
+    fn jump_to_open_bracket<E: Error>(&mut self) -> Result<(), E> {
+        let mut count = 0;
+        for i in (0..=self.program_pointer).rev() {
+            if i == 0 {
+                todo!()
+            }
+
+            match self.code[i - 1] {
+                Token::LoopIn => if count == 0 {
+                    self.program_pointer = i - 1;
+                    break;
+                } else {
+                    count -= 1;
+                }
+                Token::LoopOut => count += 1,
+                _ => (),
+            }
+        }
+
+        Ok(())
+    }
+
+    fn get_character<E: Error>(&self) -> char {
+        self.memory[self.pointer as usize] as char
+    }
+
+    fn read_character<E: Error>(&mut self, c: char) {
+        self.memory[self.pointer as usize] = c as u8;
+    }
+}
