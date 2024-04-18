a96272dd87f86a8cb0598afcb699d8b34d8bcfd3
maple8192 <57043077+maple8192@users.noreply.github.com>
2024-04-18 21:13:26 +0900

    コードを入力で与えるようにした

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::fs::read_to_string;
 use std::io::{stdin, stdout};
+use std::path::PathBuf;
+use clap::{arg, command, Parser};
 
 use crate::interpreter::interpret;
 
@@ -11,12 +14,35 @@ mod memory;
 mod error;
 mod interpreter;
 
-const PROGRAM: &str = "
-++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.
-";
+#[derive(Parser)]
+#[command(version)]
+struct Args {
+    source: Option<String>,
+
+    #[arg(short, long)]
+    file: Option<PathBuf>
+}
 
 fn main() {
-    let ret = interpret(PROGRAM, stdin(), stdout());
+    let args = Args::parse();
+    let source = match args.source {
+        Some(source) => source,
+        None => match args.file {
+            Some(path) => match read_to_string(path) {
+                Ok(source) => source,
+                Err(err) => {
+                    println!("{err}");
+                    return;
+                }
+            },
+            None => {
+                println!("needs a code or a file path");
+                return;
+            }
+        }
+    };
+
+    let ret = interpret(&source, stdin(), stdout());
     if let Err(err) = ret {
         println!("{err}");
     }
